bf0f525015a2e692302da7c304dbe6ed0abdc9d5
Raul <raul@raul.com>
2024-02-29 15:48:39 +0100

    starting main function

--- ft_printf.c
+++ ft_printf.c
@@ -1,21 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libprintf.h                                        :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ramoreno <ramoreno@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/29 12:25:17 by ramoreno          #+#    #+#             */
-/*   Updated: 2024/02/29 12:31:10 by ramoreno         ###   ########.fr       */
+/*   Created: 2024/02/29 12:39:07 by ramoreno          #+#    #+#             */
+/*   Updated: 2024/02/29 12:40:41 by ramoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifndef LIBPRINTF_H
-# define LIBPRINTF_H
-# include "./libft/libft.h"
-# include <unistd.h>
-# include <stdlib.h>
-# include <limits.h>
-int	ft_printf(char const *, ...);
-
-#endif
+#include "libprintf.h"
+/*
+int	ft_printf(char const *, ...)
+{
+}
+*/
