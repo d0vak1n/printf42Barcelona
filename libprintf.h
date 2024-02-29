be020ff5be68866140d6177720d2191161610ba0
Raul <raul@raul.com>
2024-02-29 12:32:06 +0100

    .h initiated

---
+++ libprintf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libprintf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramoreno <ramoreno@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/29 12:25:17 by ramoreno          #+#    #+#             */
+/*   Updated: 2024/02/29 12:31:10 by ramoreno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBPRINTF_H
+# define LIBPRINTF_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+
+#endif
