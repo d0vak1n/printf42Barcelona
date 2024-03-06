3071e8aa331d04345f1393fb805fa75921a96f64
Raul <raul@raul.com>
2024-03-06 13:56:32 +0100

    main function ft_printf done

--- ft_printf.c
+++ ft_printf.c
@@ -11,9 +11,38 @@
 /* ************************************************************************** */
 #include "libprintf.h"
 
+static int	_format(char const *str, void *arg)
+{
+	int	numchars;
+
+	numchars = 0;
+	if (*str == 'c')
+		numchars += ft_putchar_pf((char)arg);
+	return (numchars);
+}
+
 int	ft_printf(char const *str, ...)
 {
-	va_list args;
+	va_list	args;
+	int numchars;
+	int	i;
+
+	i = -1;
+	numchars = 0;
 	va_start(args, str);
+	while (str[++i])
+	{
+		if (str[i++] == '%')
+		{
+			if (ft_strchr("cspdiuxX", str[i]))
+				numchars += _format(&str[i], va_arg(args,  void *));
+			else if (str[i] == '%')
+				numchars += ft_putchar_pf('%');
+		}
+		else
+			numchars += ft_putchar_pf(str[i]);
+	}
+	va_end(args);
+	return (numchars);
 
 }
