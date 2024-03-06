640f8ec5f7090e54bc397010504877836c223129
Raul <raul@raul.com>
2024-03-06 13:57:37 +0100

    added putchar for ft_printf

--- ft_putchar_pf.c
+++ ft_putchar_pf.c
@@ -1,24 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libprintf.h                                        :+:      :+:    :+:   */
+/*   ft_putchar_pf.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ramoreno <ramoreno@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/29 12:25:17 by ramoreno          #+#    #+#             */
-/*   Updated: 2024/02/29 12:31:10 by ramoreno         ###   ########.fr       */
+/*   Created: 2024/03/06 10:33:37 by ramoreno          #+#    #+#             */
+/*   Updated: 2024/03/06 10:38:17 by ramoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libprintf.h"
 
-#ifndef LIBPRINTF_H
-# define LIBPRINTF_H
-# include "./libft/libft.h"
-# include <unistd.h>
-# include <stdlib.h>
-# include <limits.h>
-# include <stdarg.h>
+int	ft_putchar_pf(char const c)
+{
+	int	len;
 
-int	ft_printf(char const *str, ...);
-int	ft_putchar_pf(char const c);
-
-#endif
+	len = write(1, &c, 1);
+	if (len == -1)
+		return (-1);
+	else
+		return len;
+}
