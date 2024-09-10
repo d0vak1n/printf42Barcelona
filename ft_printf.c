bb8f553070d38a363704e6b89496fd6a3197537c
d0vak1n <rulanxa@gmail.com>
2024-09-10 15:30:21 +0200

    translated into linux

--- ft_printf.c
+++ ft_printf.c
@@ -6,57 +6,59 @@
 /*   By: ramoreno <ramoreno@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 12:39:07 by ramoreno          #+#    #+#             */
-/*   Updated: 2024/08/16 14:20:54 by ramoreno         ###   ########.fr       */
+/*   Updated: 2024/09/10 15:25:21 by ramoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	_format(char const *str, void *arg)
+#include "ft_printf.h"
+#include <stdarg.h>
+
+static int	_format(char const *str, va_list args)
 {
 	int	numchars;
 
 	numchars = 0;
 	if (*str == 'c')
-		numchars += ft_print_char(*(int *)arg);
-	if (*str == 's')
-		numchars += ft_print_string((char *)arg);
-	if (*str == 'p')
-		numchars += ft_print_pointer((unsigned long int)arg);
-	if (*str == 'd')
-		numchars += ft_print_num(*(int *)arg);
-	if (*str == 'i')
-		numchars += ft_print_num(*(int *)arg);
-	if (*str == 'u')
-		numchars += ft_print_unsigned(*(unsigned int *)arg);
-	if (*str == 'x')
-		numchars += ft_print_hex_low_or_up(*(unsigned int *)arg, 0);
-	if (*str == 'X')
-		numchars += ft_print_hex_low_or_up(*(unsigned int *)arg, 1);
+		numchars += ft_print_char((char)va_arg(args, int));
+	else if (*str == 's')
+		numchars += ft_print_string(va_arg(args, char *));
+	else if (*str == 'p')
+		numchars += ft_print_pointer(va_arg(args, unsigned long int));
+	else if (*str == 'd' || *str == 'i')
+		numchars += ft_print_num(va_arg(args, int));
+	else if (*str == 'u')
+		numchars += ft_print_unsigned(va_arg(args, unsigned int));
+	else if (*str == 'x')
+		numchars += ft_print_hex_low_or_up(va_arg(args, unsigned int), 0);
+	else if (*str == 'X')
+		numchars += ft_print_hex_low_or_up(va_arg(args, unsigned int), 1);
 	return (numchars);
 }
 
-int	ft_printf(char const *str, ...)
+int	ft_printf(char const *format, ...)
 {
 	va_list	args;
 	int		numchars;
 	int		i;
 
-	i = -1;
+	i = 0;
 	numchars = 0;
-	va_start(args, str);
-	while (str[++i])
+	va_start(args, format);
+	while (format[i])
 	{
-		if (str[i] == '%')
+		if (format[i] == '%')
 		{
-			if (ft_strchr("cspdiuxX", str[i + 1]))
-				numchars += _format(&str[i + 1], va_arg(args, void *));
-			else if (str[i + 1] == '%')
-				numchars += ft_print_char('%');
 			i++;
+			if (ft_strchr("cspdiuxX", format[i]))
+				numchars += _format(&format[i], args);
+			else if (format[i] == '%')
+				numchars += ft_print_char('%');
 		}
 		else
-			numchars += ft_print_char(str[i]);
+			numchars += ft_print_char(format[i]);
+		i++;
 	}
 	va_end(args);
 	return (numchars);
